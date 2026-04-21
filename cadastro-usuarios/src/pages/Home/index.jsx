c48f0826c3e467997d508eabd262948db2b87bf8
NatalieWF <nataliedevw@gmail.com>
2026-04-20 23:57:37 -0300

    Pasta Front end

---
+++ cadastro-usuarios/src/pages/Home/index.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState, useRef } from 'react'
+import './style.css'
+import Trash from '../../assets/lixo-eletronico.gif'
+import api from '../../services/api'
+
+function Home() {
+
+  const [users, setUsers] = useState([])  // Variável especial do React
+ 
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+
+  async function getUsers(){
+    const usersFromApi = await api.get('/usuarios')
+    
+    setUsers(usersFromApi.data)
+    console.log(usersFromApi.data)
+  }
+
+  async function createUsers(){
+    await api.post('/usuarios', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value
+    })
+    
+  }
+
+useEffect(() => {
+  getUsers()
+}, [])
+
+  return (
+
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input placeholder='Nome' name='nome' type='text' ref={inputName}/>
+        <input placeholder='Idade' name='idade' type='number' ref={inputAge}/>
+        <input placeholder='E-mail' name='email' type='email' ref={inputEmail}/>
+        <button type='button' onClick={createUsers}>Cadastro</button>
+      </form>
+
+      {users.map(user => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: <span> {user.name} </span></p>
+            <p>Idade: <span> {user.age} </span></p>
+            <p>Email: <span> {user.email} </span></p>
+          </div>
+          <button>
+            <img src={Trash}  alt="GIF" 
+  style={{ width: '20px', height: '20px', objectFit: 'cover' }} />
+          </button>
+        </div>
+      ))}
+
+      
+
+    </div>
+  )
+}
+
+
+
+
+export default Home
